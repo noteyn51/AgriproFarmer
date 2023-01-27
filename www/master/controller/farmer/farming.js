c4ecf7941d66ca8d701ede05728af5865360b771
Bunpot Klaisorn <103012749+noteyn51@users.noreply.github.com>
2023-01-27 09:46:57 +0700

    update app js

--- www/master/controller/farmer/farming.js
+++ www/master/controller/farmer/farming.js
@@ -79,14 +79,10 @@ angular
 
     };
 
-<<<<<<< HEAD
     vm.receiveLot  = function () {
       $state.go("app.receivelot")
 
     };
-=======
-
->>>>>>> a0092cd (Initial commit)
     vm.addimg = function(){
       $state.go("app.addimg")
     }
@@ -122,120 +118,7 @@ angular
       $state.go("app.receive");
     };
 
-<<<<<<< HEAD
   
 
 
-=======
-    // vm.drawstatus = false;
-    // vm.newShape = null;
-    // var all_overlays = [];
-    // var selectedShape;
-    // var map = new google.maps.Map(document.getElementById("maps"), {
-    //   center: {
-    //     lat: 13.713462,
-    //     lng: 100.478819
-    //   },
-    //   mapTypeControl: true,
-    //   streetViewControl: false,
-    //   fullscreenControl: false,
-    //   mapTypeControlOptions: {
-    //     style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
-    //     mapTypeIds: ["satellite", "roadmap", "hybrid"]
-    //   },
-    //   mapTypeId: "roadmap",
-    //   zoom: 18
-    // });
-
-    // var drawingManager = new google.maps.drawing.DrawingManager({
-    //   drawingControl: false,
-    //   drawingControlOptions: {
-    //     position: google.maps.ControlPosition.RIGHT_CENTER,
-    //     drawingModes: ["polygon"]
-    //   },
-    //   polygonOptions: {
-    //     clickable: true,
-    //     editable: true,
-    //     draggable: false,
-    //     fillColor: "red",
-    //     strokeColor: "green",
-    //     strokeWeight: 3
-    //   },
-    //   drawingMode: null
-    // });
-
-    // function clearSelection() {
-    //   if (selectedShape) {
-    //     selectedShape.setEditable(false);
-    //     selectedShape = null;
-    //   }
-    // }
-
-    // function setSelection(shape) {
-    //   clearSelection();
-    //   selectedShape = shape;
-    //   shape.setEditable(true);
-    // }
-
-    // function deleteSelectedShape() {
-    //   if (selectedShape) {
-    //     selectedShape.setMap(null);
-
-    //     vm.newShape = null;
-    //   }
-    // }
-
-    // google.maps.event.addListener(drawingManager, "polygoncomplete", function(
-    //   polygon
-    // ) {
-    //   /// Disable Controller//
-    //   drawingManager.setDrawingMode(null);
-    //   $scope.$apply(function() {
-    //     vm.newShape = polygon;
-    //   });
-
-    //   onComplete();
-
-    //   setSelection(vm.newShape);
-    // });
-
-    // function onComplete() {
-    //   google.maps.event.addListener(
-    //     vm.newShape.getPath(),
-    //     "set_at",
-    //     function() {
-    //       //console.log(vm.newShape.getPath().g);
-    //     }
-    //   );
-
-    //   //Insert point
-    //   google.maps.event.addListener(
-    //     vm.newShape.getPath(),
-    //     "insert_at",
-    //     function() {
-    //       //console.log(vm.newShape.getPath().g);
-    //     }
-    //   );
-
-    //   // add a marker at each coordinate
-
-    //   //click shape
-    //   google.maps.event.addListener(vm.newShape, "click", function(e) {
-    //     //console.log(vm.newShape);
-    //     setSelection(vm.newShape);
-    //   });
-    // }
-
-    // drawingManager.setMap(map);
-
-    // vm.delete = function() {
-    //   deleteSelectedShape();
-    //   vm.drawstatus = !vm.drawstatus;
-    // };
-
-    // vm.draw = function() {
-    //   vm.drawstatus = !vm.drawstatus;
-    //   drawingManager.setDrawingMode(google.maps.drawing.OverlayType.POLYGON);
-    // };
->>>>>>> a0092cd (Initial commit)
   });
