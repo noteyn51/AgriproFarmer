ee1337abb39a30e6b5ee549a1585e0ef72de61d8
Bunpot Klaisorn <103012749+noteyn51@users.noreply.github.com>
2023-02-22 09:59:14 +0700

    revert develop branch

--- www/master/controller/farmer/plant/startPlant.js
+++ www/master/controller/farmer/plant/startPlant.js
@@ -595,10 +595,10 @@ angular
 
       $scope.items = [];
       $scope.selected = [];
-      
-      $scope.addLot = function(){
-        $state.go('app.singleReceive')
-      }
+
+      $scope.addLot = function () {
+        $state.go("app.singleReceive");
+      };
 
       async function init() {
         let a = await getData();
